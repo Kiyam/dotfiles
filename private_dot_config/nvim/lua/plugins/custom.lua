df39f278f7c8b027a6e17f058254646bca93cf69
Kiyam <linkiyam@gmail.com>
2024-02-29 00:46:13 +0000

    added lua custom plugins

---
+++ private_dot_config/nvim/lua/plugins/custom.lua
@@ -0,0 +1,18 @@
+-- every spec file under the "plugins" directory will be loaded automatically by lazy.nvim
+--
+-- In your plugin files, you can:
+-- * add extra plugins
+-- * disable/enabled LazyVim plugins
+-- * override the configuration of LazyVim plugins
+return {
+  -- add catpuccin
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+
+  -- Configure LazyVim to load catpuccin
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+}
