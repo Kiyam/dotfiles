888e43e7945212ec1223a54ed618195aab22c9da
Kiyam <linkiyam@gmail.com>
2024-12-17 18:58:01 +0000

    as lazynvim changed from nvim-cmp to blink, modified options to use tab for autocomplete instead of enter

--- private_dot_config/nvim/lua/plugins/cmp.lua
+++ private_dot_config/nvim/lua/plugins/cmp.lua
@@ -1,14 +1,8 @@
 return {
-  {
-    "hrsh7th/nvim-cmp",
-    opts = function(_, opts)
-      local cmp = require("cmp")
-      local auto_select = false
-      opts.preselect = auto_select and cmp.PreselectMode.Item or cmp.PreselectMode.None
-      opts.completion = { completeopt = "menu,menuone,noselect" }
-      opts.mapping = cmp.mapping.preset.insert({
-        ["<CR>"] = LazyVim.cmp.confirm({ select = auto_select }),
-      })
-    end,
-  },
+	{
+		"saghen/blink.cmp",
+		opts = {
+			keymap = { preset = "super-tab" },
+		},
+	},
 }
---
+++ private_dot_config/nvim/lua/plugins/diffview.lua
@@ -0,0 +1,7 @@
+return {
+  -- add diffview
+  {
+    "sindrets/diffview.nvim",
+    lazy = true, -- we don't want to lazy load VimTeX
+  },
+}
