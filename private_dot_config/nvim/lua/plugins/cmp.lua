6bfd117fa3525e5208c49f79a9db3128bb80683a
Kiyam Lin <linkiyam@gmail.com>
2025-07-18 11:35:19 +0100

    modify blink.cmp whilst lazy doesn't update and add flutter.lua

--- private_dot_config/nvim/lua/plugins/cmp.lua
+++ private_dot_config/nvim/lua/plugins/cmp.lua
@@ -1,8 +1,20 @@
 return {
 	{
 		"saghen/blink.cmp",
+		--completion = {
+		--  list = {
+		--    selection = { preselect = false, auto_insert = false },
+		--  },
+		--},
 		opts = {
-			keymap = { preset = "super-tab" },
+			keymap = {
+				preset = "super-tab",
+				["<Tab>"] = {
+					require("blink.cmp.keymap.presets").get("super-tab")["<Tab>"][1],
+					require("lazyvim.util.cmp").map({ "snippet_forward", "ai_accept" }),
+					"fallback",
+				},
+			},
 		},
 	},
 }
